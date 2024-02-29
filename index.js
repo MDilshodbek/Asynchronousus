c8dc2a1ddbdf37a5265cfe1d7e0030f07048ab88
MDilshodbek <mdbek55@gmail.com>
2024-02-29 22:34:10 +0900

    Understanding synch vs asynch

---
+++ index.js
@@ -0,0 +1,18 @@
+let email = "Hello@gmail.com ";
+let ps = "intensive";
+
+function intoFacebook(pawsord, email, callback) {
+  callback("Sending request to beckend");
+  setTimeout(() => {
+    if ((password = "Hello from me" && email === "Hello@gmail.com")) {
+      return callback("You logged into your account");
+    } else {
+      ("Ooops! Something went wrong!");
+    }
+  }, 1000);
+}
+console.log(
+  intoFacebook(ps, email, (data) => {
+    console.log(data);
+  })
+);
